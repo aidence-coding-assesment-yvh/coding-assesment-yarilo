3c5769c9b007d8b24bc00eabf2190af1a0a70585
Yarilo Villanueva <contact@yarilovillanueva.com>
2023-10-18 14:14:12 +0200

    Initial commit
    Bootstraped with Vite: https://vitejs.dev/guide/#scaffolding-your-first-vite-project
    
    And using logo and fonts of Aidence website.

---
+++ README.md
@@ -0,0 +1,1 @@
+# Aidence Coding Assesment
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import logo from './assets/logo.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+          <img src={logo} className="logo react" alt="Aidence logo" />
+      </div>
+   
+    </>
+  )
+}
+
+export default App
