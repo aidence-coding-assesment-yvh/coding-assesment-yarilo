49faae18afbe9452c2da2680c6671c9d3aebf205
Yarilo Villanueva <contact@yarilovillanueva.com>
2023-10-18 16:22:11 +0200

    Format <App /> component with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,36 @@
-import logo from './assets/logo.svg'
-import './App.css'
-import { useEffect, useState } from 'react'
-import ProviderRequest from './providers/request'
-import { ENDPOINTS } from './constants'
-import User from './types/User'
+import logo from "./assets/logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import ProviderRequest from "./providers/request";
+import { ENDPOINTS } from "./constants";
+import User from "./types/User";
 
 function App() {
-
   const [users, setUsers] = useState<User[]>();
-  const [error, setError] = useState<string>('');
+  const [error, setError] = useState<string>("");
 
-const fetchUsers = async () => {
-  try {
-    const users = await ProviderRequest.get(ENDPOINTS.USERS);
-    setUsers(users);
-  } catch(error) {
-    console.error(`Error trying to fetch users: ${error}`);
-    setError(String(error));
-  }
-}
-  
+  const fetchUsers = async () => {
+    try {
+      const users = await ProviderRequest.get(ENDPOINTS.USERS);
+      setUsers(users);
+    } catch (error) {
+      console.error(`Error trying to fetch users: ${error}`);
+      setError(String(error));
+    }
+  };
+
   useEffect(() => {
-      fetchUsers();
+    fetchUsers();
   }, []);
-  
+
   return (
     <>
       <div>
-          <img src={logo} className="logo react" alt="Aidence logo" />
-         {error? <p>{error}</p> : null}
+        <img src={logo} className="logo react" alt="Aidence logo" />
+        {error ? <p>{error}</p> : null}
       </div>
-   
     </>
-  )
+  );
 }
 
-export default App
+export default App;
