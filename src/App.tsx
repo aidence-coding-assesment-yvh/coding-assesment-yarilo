d8f33054e5ffcaeb583bc27dc31034a5f5c4028d
Yarilo Villanueva <contact@yarilovillanueva.com>
2023-10-18 17:49:42 +0200

    Filter user by name or email
    Adding a new component with an input and a small filter function that runs on every render before displaying the markup.

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,23 @@ import ProviderRequest from "./providers/request";
 import { ENDPOINTS } from "./constants";
 import User from "./types/User";
 import Table from "./components/Table";
+import FilterInput from "./components/FilterInput";
+
+const filterUsers = (users: User[], filter: string): User[] => {
+  if (!filter) return users;
+  return users.filter((user) => {
+    const matchesName = user.name.toLowerCase().includes(filter.toLowerCase());
+    const matchesEmail = user.email
+      .toLowerCase()
+      .includes(filter.toLowerCase());
+    return matchesName || matchesEmail;
+  });
+};
 
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState<string>("");
+  const [filter, setFilter] = useState<string>("");
 
   const fetchUsers = async () => {
     try {
@@ -24,12 +37,20 @@ function App() {
     fetchUsers();
   }, []);
 
+  const onChangeFilter = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFilter(event.target.value);
+  };
+
+  const filteredUsers = filterUsers(users, filter);
   return (
     <>
       <div className="app">
         <img src={logo} className="logo react" alt="Aidence logo" />
-        <Table users={users} />
-        {error ? <p>{error}</p> : null}
+        <div className="content">
+          <FilterInput onChange={onChangeFilter} />
+          <Table users={filteredUsers} />
+          {error ? <p>{error}</p> : null}
+        </div>
       </div>
     </>
   );
