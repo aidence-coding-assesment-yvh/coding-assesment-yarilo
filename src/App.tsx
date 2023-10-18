d65bbb4ef791a73662ea5c6dfde78c804c61991d
Yarilo Villanueva <contact@yarilovillanueva.com>
2023-10-18 16:11:11 +0200

    Fetch users from the endpoint
    In a real app I'd probably use an already standard library for this(https://swr.vercel.app/ or https://tanstack.com/query/latest/docs/react/overview for example) to handle edge cases, caching and so on.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
 import logo from './assets/logo.svg'
 import './App.css'
+import { useEffect, useState } from 'react'
+import ProviderRequest from './providers/request'
+import { ENDPOINTS } from './constants'
+import User from './types/User'
 
 function App() {
 
+  const [users, setUsers] = useState<User[]>();
+  const [error, setError] = useState<string>('');
+
+const fetchUsers = async () => {
+  try {
+    const users = await ProviderRequest.get(ENDPOINTS.USERS);
+    setUsers(users);
+  } catch(error) {
+    console.error(`Error trying to fetch users: ${error}`);
+    setError(String(error));
+  }
+}
+  
+  useEffect(() => {
+      fetchUsers();
+  }, []);
+  
   return (
     <>
       <div>
           <img src={logo} className="logo react" alt="Aidence logo" />
+         {error? <p>{error}</p> : null}
       </div>
    
     </>
